89395846397a10fad75771eb66d69ae7910be43b
mattia richetto <mrichetto@opentable.com>
2016-12-10 22:17:18 -0800

    fix server port for heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 app.use(express.static('build'));
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`navigate to: http://127.0.0.1:${port}`); // eslint-disable-line no-console
 });
