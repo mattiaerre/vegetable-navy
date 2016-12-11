b494dcd98cd60cd9d2d80be7f1b31a16b4ee21ef
mattia richetto <mrichetto@opentable.com>
2016-12-10 22:19:30 -0800

    update server.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 
 const app = express();
+
 app.use(express.static('build'));
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`navigate to: http://127.0.0.1:${port}`); // eslint-disable-line no-console
-});
+
+app.listen(port);
