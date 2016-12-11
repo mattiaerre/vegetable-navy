b681bedbf0d0875de8b3d1ce34cce1edf611ec9c
mattia richetto <mrichetto@opentable.com>
2016-12-10 23:19:09 -0800

    add Layout

---
+++ src/components/Layout.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Layout.css';
+
+const Layout = () => { // eslint-disable-line arrow-body-style
+  return (
+    <div>
+      <h1>vegetable-navy</h1>
+      <span>Yet another React, Redux, RxJS playground app</span>
+    </div>
+  );
+};
+
+export default Layout;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,13 @@
-/* eslint-disable import/default */
-
 import React from 'react';
-import {render} from 'react-dom';
+import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { Router, browserHistory } from 'react-router';
+import { syncHistoryWithStore } from 'react-router-redux';
 import routes from './routes';
 import configureStore from './store/configureStore';
-require('./favicon.ico'); // Tell webpack to load favicon.ico
-import './styles/styles.scss'; // Yep, that's right. You can import SASS/CSS files too! Webpack will run the associated loader and plug this into the page.
-import { syncHistoryWithStore } from 'react-router-redux';
 
 const store = configureStore();
 
-// Create an enhanced history that syncs navigation events with the store
 const history = syncHistoryWithStore(browserHistory, store);
 
 render(
